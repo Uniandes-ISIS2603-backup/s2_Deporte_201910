66f740469dfe538fce126ab0c00e59fee7078350
Niolas Poch <n.poch@uniandes.edu.co>
2019-02-10 23:14:35 -0500

    Clases DTO(Cliente,Equipo,Partido) Clases DetailDTO(Cliente,Equipo,Partido) Clasese Resource(Cliente,Equipo,Partido)

---
+++ s2_deporte-api/src/main/java/co/edu/uniandes/csw/deporte/resources/PartidoResource.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.deporte.resources;
+
+import java.util.logging.Logger;
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+/**
+ *
+ * @author estudiante
+ */
+@Path("partidos")
+@Produces("application/json")
+@Consumes("application/json")
+@RequestScoped
+public class PartidoResource 
+{
+    private static final Logger LOGGER = Logger.getLogger(PartidoResource.class.getName());
+}
