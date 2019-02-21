3925000712a5633c99af40054abd1e6f59c9701e
Juan Camilo Garcia <estudiante@ISIS2603-430.sis.virtual.uniandes.edu.co>
2019-02-21 09:15:38 -0500

    .

--- s2_deporte-back/src/main/java/co/edu/uniandes/csw/deporte/persistence/CampeonatoPersistence.java
+++ s2_deporte-back/src/main/java/co/edu/uniandes/csw/deporte/persistence/CampeonatoPersistence.java
@@ -26,9 +26,7 @@ public class CampeonatoPersistence {
     protected EntityManager em;
     public CampeonatoEntity create(CampeonatoEntity campeonatoEntity) {
         LOGGER.log(Level.INFO, "Creando un campeonato nuevo");
-        /* Note que hacemos uso de un método propio de EntityManager para persistir la editorial en la base de datos.
-        Es similar a "INSERT INTO table_name (column1, column2, column3, ...) VALUES (value1, value2, value3, ...);" en SQL.
-         */
+
         em.persist(campeonatoEntity);
         LOGGER.log(Level.INFO, "Saliendo de crear un campeonato nuevo");
         return campeonatoEntity;
@@ -36,39 +34,27 @@ public class CampeonatoPersistence {
     
     public List<CampeonatoEntity> findAll() {
         LOGGER.log(Level.INFO, "Consultando todos los campeonatos");
-        // Se crea un query para buscar todas las editoriales en la base de datos.
         TypedQuery query = em.createQuery("select u from CampeonatoEntity u", CampeonatoEntity.class);
-        // Note que en el query se hace uso del método getResultList() que obtiene una lista de editoriales.
         return query.getResultList();
     }
     
     public CampeonatoEntity find(Long campeonatoId) {
         LOGGER.log(Level.INFO, "Consultando campeonato con id={0}", campeonatoId);
-        /* Note que se hace uso del metodo "find" propio del EntityManager, el cual recibe como argumento 
-        el tipo de la clase y el objeto que nos hara el filtro en la base de datos en este caso el "id"
-        Suponga que es algo similar a "select * from EditorialEntity where id=id;" - "SELECT * FROM table_name WHERE condition;" en SQL.
-         */
+
         return em.find(CampeonatoEntity.class, campeonatoId);
     }
    
      public CampeonatoEntity update(CampeonatoEntity campeonatoEntity) {
         LOGGER.log(Level.INFO, "Actualizando campeonato con id = {0}", campeonatoEntity.getId());
-        /* Note que hacemos uso de un método propio del EntityManager llamado merge() que recibe como argumento
-        la editorial con los cambios, esto es similar a 
-        "UPDATE table_name SET column1 = value1, column2 = value2, ... WHERE condition;" en SQL.
-         */
+
         LOGGER.log(Level.INFO, "Saliendo de actualizar el campeonato con id = {0}", campeonatoEntity.getId());
         return em.merge(campeonatoEntity);
     }
      
       public void delete(Long campeonatoId) {
         LOGGER.log(Level.INFO, "Borrando editorial con id = {0}", campeonatoId);
-        // Se hace uso de mismo método que esta explicado en public EditorialEntity find(Long id) para obtener la editorial a borrar.
         CampeonatoEntity entity = em.find(CampeonatoEntity.class, campeonatoId);
-        /* Note que una vez obtenido el objeto desde la base de datos llamado "entity", volvemos hacer uso de un método propio del
-         EntityManager para eliminar de la base de datos el objeto que encontramos y queremos borrar.
-         Es similar a "delete from EditorialEntity where id=id;" - "DELETE FROM table_name WHERE condition;" en SQL.*/
-        em.remove(entity);
+               em.remove(entity);
         LOGGER.log(Level.INFO, "Saliendo de borrar la editorial con id = {0}", campeonatoId);
     }
       
---
+++ s2_deporte-back/src/test/java/co/edu/uniandes/csw/deporte/test/persistence/CampeonatoPersistenceTest.java
@@ -0,0 +1,191 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.deporte.test.persistence;
+
+import co.edu.uniandes.csw.deporte.entities.CampeonatoEntity;
+import co.edu.uniandes.csw.deporte.persistence.CampeonatoPersistence;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+import junit.framework.Assert;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
+/**
+ *
+ * @author estudiante
+ */
+@RunWith(Arquillian.class)
+public class CampeonatoPersistenceTest {
+    @Inject
+    CampeonatoPersistence campeonatoPersistence;
+    @PersistenceContext
+    private EntityManager em;
+    
+    @Inject
+    UserTransaction utx;
+    
+    private List<CampeonatoEntity> data = new ArrayList<>();
+    @Deployment
+    public static JavaArchive createDeployment()
+    {
+       return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(CampeonatoEntity.class.getPackage())
+                .addPackage(CampeonatoPersistence.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");    
+    }
+    
+     /**
+     * Configuración inicial de la prueba.
+     */
+    @Before
+    public void configTest() {
+        try {
+            utx.begin();
+            em.joinTransaction();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+ /**
+     * Limpia las tablas que están implicadas en la prueba.
+     */
+    private void clearData() {
+        em.createQuery("delete from CampeonatoEntity").executeUpdate();
+    }
+
+    /**
+     * Inserta los datos iniciales para el correcto funcionamiento de las
+     * pruebas.
+     */
+    private void insertData() {
+        PodamFactory factory = new PodamFactoryImpl();
+        for (int i = 0; i < 3; i++) {
+            CampeonatoEntity entity = factory.manufacturePojo(CampeonatoEntity.class);
+
+            em.persist(entity);
+            data.add(entity);
+            
+        }
+    }
+    /**
+     * Prueba para crear un Campeonato.
+     */
+    @Test
+    public void createCampeonatoTest()
+    {
+        PodamFactory factory = new PodamFactoryImpl();
+        CampeonatoEntity newEntity;
+        newEntity = factory.manufacturePojo(CampeonatoEntity.class);
+        CampeonatoEntity result = campeonatoPersistence.create(newEntity);
+        
+        Assert.assertNotNull(result);
+        
+        CampeonatoEntity entity = em.find(CampeonatoEntity.class, result.getId());
+        
+        Assert.assertEquals(newEntity.getNombre(), entity.getNombre());
+        
+            }
+  /**
+     * Prueba para consultar la lista de Campeonatos.
+     */
+   
+    @Test
+    public void getCampeonatosTest()
+    {
+        List<CampeonatoEntity> list = campeonatoPersistence.findAll();
+        Assert.assertEquals(data.size(), list.size());
+        for(CampeonatoEntity enti : list)
+        {
+            boolean found = false;
+            for(CampeonatoEntity entity : data)
+            {
+                if(enti.getId().equals(entity.getId()))
+                {
+                    found = true;
+                }
+            }
+            Assert.assertTrue(found);
+        }
+        
+    }
+    
+    /**
+     * Prueba para consultar un Campeonato.
+     */
+    @Test
+    public void getCampeonatoTest() {
+        CampeonatoEntity entity = data.get(0);
+        
+        CampeonatoEntity newEntity = campeonatoPersistence.find(entity.getId());
+        Assert.assertNotNull(newEntity);
+        Assert.assertEquals(entity.getNombre(), newEntity.getNombre());
+        
+    }
+    
+    /**
+     * Prueba para eliminar un Campeonato.
+     */
+    @Test
+    public void deleteCampeonatoTest() {
+        CampeonatoEntity entity = data.get(0);
+        campeonatoPersistence.delete(entity.getId());
+        CampeonatoEntity deleted = em.find(CampeonatoEntity.class, entity.getId());
+        Assert.assertNull(deleted);
+    }
+    
+    /**
+     * Prueba para actualizar un Campeonato.
+     */
+    @Test
+    public void updateCampeonatoTest() {
+        CampeonatoEntity entity = data.get(0);
+        PodamFactory factory = new PodamFactoryImpl();
+        CampeonatoEntity newEntity = factory.manufacturePojo(CampeonatoEntity.class);
+
+        newEntity.setId(entity.getId());
+
+        campeonatoPersistence.update(newEntity);
+
+        CampeonatoEntity resp = em.find(CampeonatoEntity.class, entity.getId());
+
+        Assert.assertEquals(newEntity.getNombre(), resp.getNombre());
+    
+    }
+    
+     /**
+     * Prueba para consultasr un Campeonato por nombre.
+     */
+    @Test
+    public void findCampeonatoByNombreTest() {
+        CampeonatoEntity entity = data.get(0);
+        CampeonatoEntity newEntity = campeonatoPersistence.findByName(entity.getNombre());
+        Assert.assertNotNull(newEntity);
+        Assert.assertEquals(entity.getNombre(), newEntity.getNombre());
+
+        newEntity = campeonatoPersistence.findByName(null);
+        Assert.assertNull(newEntity);
+    }
+}
