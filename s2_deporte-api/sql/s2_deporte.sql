e8109766e5c7da194a61644bd01795549f848c67
Niolas Poch <n.poch@uniandes.edu.co>
2019-03-08 23:15:26 -0500

    varios

--- s2_deporte-api/sql/s2_deporte.sql
+++ s2_deporte-api/sql/s2_deporte.sql
@@ -12,7 +12,6 @@ delete from PropietarioEntity;
 delete from ReservaEntity;
 delete from FranjaEntity;
 
-select * from CampeonatoEntity;
 
 insert into ClienteEntity (id,nombre) values (0, 'Santiago Serrano');
 insert into ClienteEntity (id,nombre) values (1, 'Juan Camilo Garcia');
@@ -20,7 +19,4 @@ insert into ClienteEntity (id,nombre) values (2, 'Santiago Barbosa');
 insert into ClienteEntity (id,nombre) values (3, 'Nicolas de la Hoz');
 insert into ClienteEntity (id,nombre) values (4, 'Nicolas Poch');
 
-
-insert intp BlogEntity (id, descripcion, identificador, nombre, campeonato_id) values (0, 'Las location services', 0, 'Desarrollo', 0);
-insert intp BlogEntity (id, descripcion, identificador, nombre, campeonato_id) values (1, 'Te la creiste wey', 1, 'JAJAJA', 1);
 
