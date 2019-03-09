a0fda5bed34805a3e0b7fe5ff161db5270f25d02
Santiago Barbosa <s.barbosan@uniandes.edu.co>
2019-03-08 23:03:04 -0500

    Issues resueltos Franja, Agenda

--- s2_deporte-api/src/main/java/co/edu/uniandes/csw/deporte/resources/AgendaResourse.java
+++ s2_deporte-api/src/main/java/co/edu/uniandes/csw/deporte/resources/AgendaResourse.java
@@ -6,43 +6,67 @@
 package co.edu.uniandes.csw.deporte.resources;
 
 import co.edu.uniandes.csw.deporte.dtos.AgendaDTO;
+import co.edu.uniandes.csw.deporte.ejb.AgendaLogic;
+import co.edu.uniandes.csw.deporte.entities.AgendaEntity;
+import co.edu.uniandes.csw.deporte.exceptions.BusinessLogicException;
 import java.util.logging.Logger;
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 
 /**
  *
  * @author Santiago Barbosa
  */
-
 @Path("agendas")
 @Produces("application/json")
 @Consumes("application/json")
 @RequestScoped
 public class AgendaResourse {
-    
+
     private static final Logger LOGGER = Logger.getLogger(AgendaResourse.class.getName());
-    
+
+    @Inject
+    AgendaLogic logica;
     @POST
-    @Path("{agendaId : \\d+}")
-    public AgendaDTO createPropietario(AgendaDTO agenda){
-        return agenda;
+    public AgendaDTO createAgenda(AgendaDTO agenda) throws BusinessLogicException{
+        AgendaEntity entity = agenda.toEntity();
+        entity = logica.create(entity);
+        return new AgendaDTO(entity);
     }
-    
+
     @PUT
-    @Path("{agendaId : \\d+}")
-    public AgendaDTO modifyCancha(AgendaDTO agenda){
+    public AgendaDTO modifyAgenda(AgendaDTO agenda) {
         return agenda;
     }
-    
+
     @DELETE
     @Path("{agendaId : \\d+}")
-    public AgendaDTO deleteCancha(AgendaDTO agenda){
-        return agenda;
+    public void deleteAgenda(@PathParam("agendaId")Long id) throws WebApplicationException, BusinessLogicException{
+        AgendaEntity entity = logica.find(id);
+        if(entity == null)
+        {
+            throw new WebApplicationException("Agenda con id: " + id + " no existe", 404);
+        }
+        logica.delete(id);
+    }
+    
+    @GET
+    @Path("{agendaId : \\d+}")
+    public AgendaDTO getAgenda(@PathParam("agendaId") Long id) throws WebApplicationException, BusinessLogicException{
+        AgendaEntity entity = logica.find(id);
+        if(entity == null){
+            throw new WebApplicationException("Agenda con id: " + id + " no existe", 404);
+        }
+        return new AgendaDTO(entity);
+        
     }
 }
--- s2_deporte-api/src/main/java/co/edu/uniandes/csw/deporte/resources/FranjaResource.java
+++ s2_deporte-api/src/main/java/co/edu/uniandes/csw/deporte/resources/FranjaResource.java
@@ -6,6 +6,9 @@
 package co.edu.uniandes.csw.deporte.resources;
 
 import co.edu.uniandes.csw.deporte.dtos.FranjaDTO;
+import co.edu.uniandes.csw.deporte.ejb.FranjaLogic;
+import co.edu.uniandes.csw.deporte.entities.FranjaEntity;
+import co.edu.uniandes.csw.deporte.exceptions.BusinessLogicException;
 import java.util.logging.Logger;
 import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.Consumes;
@@ -14,35 +17,49 @@ import javax.ws.rs.GET;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import java.util.Date;
-
+import javax.inject.Inject;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.WebApplicationException;
 
 /**
  *
  * @author Santiago Barbosa
  */
-
-@Path ("franjas")
+@Path("franjas")
 @Produces("application/json")
 @Consumes("application/json")
 @RequestScoped
 public class FranjaResource {
+
     private static final Logger LOGGER = Logger.getLogger(FranjaResource.class.getName());
-    
+
+    @Inject
+    FranjaLogic logica;
+
     @POST
-    @Path("{franjaId : \\d+}")
-    public FranjaDTO createFranja(FranjaDTO franja){
-        return franja;
+    public FranjaDTO createFranja(FranjaDTO franja) throws BusinessLogicException {
+        FranjaEntity entity = franja.toEntity();
+        entity = logica.create(entity);
+        return new FranjaDTO(entity);
     }
-    
+
     @GET
     @Path("{franjaId : \\d+}")
-    public FranjaDTO getFranja(){
-        return null;
+    public FranjaDTO getFranja(@PathParam("franjaId") Long id) throws WebApplicationException, BusinessLogicException {
+        FranjaEntity entity = logica.find(id);
+        if (entity == null) {
+            throw new WebApplicationException("Franja con id: " + id + " no existe", 404);
+        }
+        return new FranjaDTO(entity);
     }
+
     @DELETE
     @Path("{franjaId : \\d+}")
-    public FranjaDTO deleteFranja(FranjaDTO franja){
-        return franja;
+    public void deleteFranja(@PathParam("franjaId") Long id) throws WebApplicationException, BusinessLogicException {
+        FranjaEntity entity = logica.find(id);
+        if (entity == null) {
+            throw new WebApplicationException("Franja con id: " + id + " no existe", 404);
+        }
+        logica.delete(id);
     }
 }
